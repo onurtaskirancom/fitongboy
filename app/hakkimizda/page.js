2435f654c55b395d9f2ec589efecdd066b74a48c
onurtaskirancom <taskiranonur9@gmail.com>
2024-08-07 01:47:21 +0300

    added about page

---
+++ app/hakkimizda/page.js
@@ -0,0 +1,33 @@
+'use client';
+
+export default function About() {
+  return (
+    <div className="max-w-screen-lg mx-auto p-4 mt-16 bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
+      <h1 className="text-3xl font-bold text-center mb-8">Hakkımızda</h1>
+      <div className="space-y-4">
+        <p>
+          <strong>Fitongboy</strong> olarak fitness, vücut geliştirme, güç
+          antrenmanı ve makale paylaşımı konusunda uzmanlaşmış bir platformuz.
+          Amacımız, sizlere en güncel ve doğru bilgileri sunarak, sağlıklı ve
+          güçlü bir vücuda ulaşmanızda yardımcı olmaktır.
+        </p>
+        <p>
+          Sitemizde çeşitli antrenman programları, beslenme önerileri ve fitness
+          hakkında detaylı makaleler bulabilirsiniz. İster yeni başlayan olun,
+          ister ileri seviye bir sporcu, herkes için uygun içerikler
+          sunmaktayız.
+        </p>
+        <p>
+          Ayrıca, kişiye özel antrenman programları ve danışmanlık hizmetleri de
+          sunuyoruz. İhtiyaçlarınıza ve hedeflerinize uygun bir program
+          oluşturmak için bizimle iletişime geçebilirsiniz.
+        </p>
+        <p>
+          Sağlıklı yaşam yolculuğunuzda <strong>Fitongboy</strong> olarak
+          yanınızdayız. Hedeflerinize ulaşmanız için gereken motivasyonu ve
+          bilgiyi sizlere sağlamak için buradayız.
+        </p>
+      </div>
+    </div>
+  );
+}
