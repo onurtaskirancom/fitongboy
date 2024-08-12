487a6c67717f97e0b6edd724dc4369ca84d37a91
onurtaskirancom <taskiranonur9@gmail.com>
2024-08-12 06:20:53 +0300

    robots sitemap and site title description added

--- app/rss.xml.js
+++ app/rss.xml.js
@@ -1,7 +1,7 @@
 import { generateRSSFeed } from '../api/rss/route';
 
 export async function GET(req) {
-  const rssFeed = await generateRSSFeed(); // RSS beslemesini oluştur
+  const rssFeed = await generateRSSFeed();
   return new Response(rssFeed, {
     headers: {
       'Content-Type': 'application/xml',
